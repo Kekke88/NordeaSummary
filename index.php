93e13e7fb79c2e18a5bac5d508f50304514f3ea9
Henric Johansson <henric.johansson88@gmail.com>
2014-11-20 23:40:03 +0100

    First commit
    Work in progress

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+		<title>Nordea Parser</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<form action="controller/ParseData.php" method="post">
+			<label for="ExportFile">Choose your export file:</label><br />
+			<input type="file" name="ExportFile" /><br /><br />
+			<input type="submit" value="Submit" /><br />
+		</form>
+	</body>
+</html>
